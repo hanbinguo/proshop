a7107e2e1b887014b84f889ffea3867fdf48cabf
hanbinguo <hanbinguo14@outlook.com>
2023-11-13 13:45:09 +0100

    commit sometime

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+
 const App = () => {
 	return (
 		<>
-			<h1>Welcome To ProShop</h1>
+			<Header />
+			<main className="py-3">
+				<Container>
+					<h1>Welcome To Bouquiniste</h1>
+				</Container>
+			</main>
 		</>
 	);
 };
