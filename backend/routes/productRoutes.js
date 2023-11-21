c8e83b92705060ba3024f885c0df62eb7b8e5919
hanbinguo <hanbinguo14@outlook.com>
2023-11-21 12:54:10 +0100

    commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,29 @@
+//this is a file where we put all of our product routes and inject them into server.js
+import express from "express";
+const router = express.Router();
+
+//asyncHandler
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
+
+router.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+		if (product) {
+			res.json(product);
+		} else {
+			res.status(404);
+			throw new Error("Resource not found");
+		}
+	})
+);
+export default router;
