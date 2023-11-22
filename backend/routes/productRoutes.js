e32c3023c3c30cfee041ae05070bbee39cc958f7
hanbinguo <hanbinguo14@outlook.com>
2023-11-22 09:29:14 +0100

    add controller

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,27 +3,12 @@ import express from "express";
 const router = express.Router();
 
 //asyncHandler
-import asyncHandler from "../middleware/asyncHandler.js";
-import Product from "../models/productModel.js";
 
-router.get(
-	"/",
-	asyncHandler(async (req, res) => {
-		const products = await Product.find({});
-		res.json(products);
-	})
-);
+import {
+	getProduct,
+	getProductById,
+} from "../controllers/productController.js";
+router.route("/").get(getProduct);
+router.route("/:id").get(getProductById);
 
-router.get(
-	"/:id",
-	asyncHandler(async (req, res) => {
-		const product = await Product.findById(req.params.id);
-		if (product) {
-			res.json(product);
-		} else {
-			res.status(404);
-			throw new Error("Resource not found");
-		}
-	})
-);
 export default router;
