9a7c9b5014a2284ad795725d5c87816d148215ac
hanbinguo <hanbinguo14@outlook.com>
2023-11-27 00:46:50 +0100

    this is the drunk version but ist works this is the update of the single-product-query update

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,28 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
-// import products from "../products";
-//now we are gonna fetch data this way
-import axios from "axios";
-import { useState, useEffect } from "react";
+import { useGetProductsQuery } from "../slices/productsApiSlice";
 
 function HomeScreen() {
-	const [products, setProducts] = useState([]);
-	useEffect(() => {
-		const fetchProducts = async () => {
-			const { data } = await axios.get("/api/products");
-			setProducts(data);
-		};
-
-		fetchProducts();
-	}, []);
+	const { data: products, isLoading, error } = useGetProductsQuery();
 	return (
 		<>
-			<h1>Latest Products</h1>
-			<Row>
-				{products.map((product) => (
-					<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-						<Product product={product} />
-					</Col>
-				))}
-			</Row>
+			{isLoading ? (
+				<h2>Loading...</h2>
+			) : error ? (
+				<div>{error?.data?.message || error.error}</div>
+			) : (
+				<>
+					<h1>Latest Products</h1>
+					<Row>
+						{products.map((product) => (
+							<Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+								<Product product={product} />
+							</Col>
+						))}
+					</Row>
+				</>
+			)}
 		</>
 	);
 }
