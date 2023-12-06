b551569054a103db17084d416eb6a8cb5fa2be61
hanbinguo <hanbinguo14@outlook.com>
2023-12-06 20:16:03 +0100

    user_auth_endpoint

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,8 @@ console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 connectDB();
 
 const app = express();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => {
